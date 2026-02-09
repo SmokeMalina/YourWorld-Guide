4be298b86472bfe786821b7f8e6e70b2d6b92f27
SmokeMalina <stels334@gmail.com>
2026-02-09 17:06:56 +0300

    22

---
+++ craft/recipes.js
@@ -0,0 +1,27 @@
+window.RECIPES = [
+  {
+    title: "Черный кейс 100",
+    path: "/YourWorld-Guide/craft/cases/case_black_100.html",
+    icon: "/YourWorld-Guide/images/categories/hdd_case.png"
+  },
+  {
+    title: "Синий кейс 100",
+    path: "/YourWorld-Guide/craft/cases/case_blue_100.html",
+    icon: "/YourWorld-Guide/images/categories/hdd_case.png"
+  },
+  {
+    title: "Зелёный кейс 100",
+    path: "/YourWorld-Guide/craft/cases/case_green_100.html",
+    icon: "/YourWorld-Guide/images/categories/hdd_case.png"
+  },
+  {
+    title: "Красный кейс 100",
+    path: "/YourWorld-Guide/craft/cases/case_red_100.html",
+    icon: "/YourWorld-Guide/images/categories/hdd_case.png"
+  },
+  {
+    title: "Reload кейс 100",
+    path: "/YourWorld-Guide/craft/cases/reload_case_100.html",
+    icon: "/YourWorld-Guide/images/categories/hdd_case.png"
+  }
+];
