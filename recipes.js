2ffd781f348d94e4ca55bc8bfff1334e4c59879f
SmokeMalina <stels334@gmail.com>
2026-02-09 16:44:44 +0300

    2

--- recipes.js
+++ recipes.js
@@ -1,17 +1,13 @@
-const RECIPES = [
-  {
-    title: "Черный кейс 100",
-    path: "/YourWorld-Guide/craft/cases/case_black_100.html",
-    category: "cases"
-  },
-  {
-    title: "Синий кейс 100",
-    path: "/YourWorld-Guide/craft/cases/case_blue_100.html",
-    category: "cases"
-  },
-  {
-    title: "Красный кейс 100",
-    path: "/YourWorld-Guide/craft/cases/case_red_100.html",
-    category: "cases"
-  }
+window.RECIPES = [
+  { title: "Черный кейс 100", path: "/YourWorld-Guide/craft/cases/case_black_100.html" },
+  { title: "Синий кейс 100", path: "/YourWorld-Guide/craft/cases/case_blue_100.html" },
+  { title: "Зеленый кейс 100", path: "/YourWorld-Guide/craft/cases/case_green_100.html" },
+  { title: "Голубой кейс 100", path: "/YourWorld-Guide/craft/cases/case_lightblue_100.html" },
+  { title: "Оранжевый кейс 100", path: "/YourWorld-Guide/craft/cases/case_orange_100.html" },
+  { title: "Розовый кейс 100", path: "/YourWorld-Guide/craft/cases/case_pink_100.html" },
+  { title: "Фиолетовый кейс 100", path: "/YourWorld-Guide/craft/cases/case_purple_100.html" },
+  { title: "Красный кейс 100", path: "/YourWorld-Guide/craft/cases/case_red_100.html" },
+  { title: "Белый кейс 100", path: "/YourWorld-Guide/craft/cases/case_white_100.html" },
+  { title: "Желтый кейс 100", path: "/YourWorld-Guide/craft/cases/case_yellow_100.html" },
+  { title: "Релоад кейс 100", path: "/YourWorld-Guide/craft/cases/reload_case_100.html" }
 ];
