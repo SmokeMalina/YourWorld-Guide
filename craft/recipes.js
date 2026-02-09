c2f6fed45c16bcdb5c8466555dfd68678e8137d8
SmokeMalina <stels334@gmail.com>
2026-02-09 17:45:22 +0300

    2

--- craft/recipes.js
+++ craft/recipes.js
@@ -2,26 +2,56 @@ window.RECIPES = [
   {
     title: "Черный кейс 100",
     path: "/YourWorld-Guide/craft/cases/case_black_100.html",
-    icon: "/YourWorld-Guide/images/categories/hdd_case.png"
+    icon: "/YourWorld-Guide/images/case/case_black.png"
   },
   {
     title: "Синий кейс 100",
     path: "/YourWorld-Guide/craft/cases/case_blue_100.html",
-    icon: "/YourWorld-Guide/images/categories/hdd_case.png"
+    icon: "/YourWorld-Guide/images/case/case_blue.png"
   },
   {
     title: "Зелёный кейс 100",
     path: "/YourWorld-Guide/craft/cases/case_green_100.html",
-    icon: "/YourWorld-Guide/images/categories/hdd_case.png"
+    icon: "/YourWorld-Guide/images/case/case_green.png"
+  },
+  {
+    title: "Голубой кейс 100",
+    path: "/YourWorld-Guide/craft/cases/case_lightblue_100.html",
+    icon: "/YourWorld-Guide/images/case/case_lightblue.png"
+  },
+  {
+    title: "Оранжевый кейс 100",
+    path: "/YourWorld-Guide/craft/cases/case_orange_100.html",
+    icon: "/YourWorld-Guide/images/case/case_orange.png"
+  },
+  {
+    title: "Розовый кейс 100",
+    path: "/YourWorld-Guide/craft/cases/case_pink_100.html",
+    icon: "/YourWorld-Guide/images/case/case_pink.png"
+  },
+  {
+    title: "Фиолетовый кейс 100",
+    path: "/YourWorld-Guide/craft/cases/case_purple_100.html",
+    icon: "/YourWorld-Guide/images/case/case_purple.png"
   },
   {
     title: "Красный кейс 100",
     path: "/YourWorld-Guide/craft/cases/case_red_100.html",
-    icon: "/YourWorld-Guide/images/categories/hdd_case.png"
+    icon: "/YourWorld-Guide/images/case/case_red.png"
   },
   {
-    title: "Reload кейс 100",
+    title: "Белый кейс 100",
+    path: "/YourWorld-Guide/craft/cases/case_white_100.html",
+    icon: "/YourWorld-Guide/images/case/case_white.png"
+  },
+  {
+    title: "Желтый кейс 100",
+    path: "/YourWorld-Guide/craft/cases/case_yellow_100.html",
+    icon: "/YourWorld-Guide/images/case/case_yellow.png"
+  },
+  {
+    title: "Кейс для патронов 100",
     path: "/YourWorld-Guide/craft/cases/reload_case_100.html",
-    icon: "/YourWorld-Guide/images/categories/hdd_case.png"
+    icon: "/YourWorld-Guide/images/case/case_reload.png"
   }
 ];
