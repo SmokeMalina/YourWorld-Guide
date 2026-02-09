9361ac4cb1246853674382ae7f730594a1cbdcc7
SmokeMalina <34610558+SmokeMalina@users.noreply.github.com>
2026-02-09 16:34:52 +0300

    Create recipes.js

---
+++ recipes.js
@@ -0,0 +1,17 @@
+const RECIPES = [
+  {
+    title: "Черный кейс 100",
+    path: "/YourWorld-Guide/craft/cases/case_black_100.html",
+    category: "cases"
+  },
+  {
+    title: "Синий кейс 100",
+    path: "/YourWorld-Guide/craft/cases/case_blue_100.html",
+    category: "cases"
+  },
+  {
+    title: "Красный кейс 100",
+    path: "/YourWorld-Guide/craft/cases/case_red_100.html",
+    category: "cases"
+  }
+];
