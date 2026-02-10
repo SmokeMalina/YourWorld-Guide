49bad31d07635caf6c7d77cb02abff003a38cc0a
SmokeMalina <stels334@gmail.com>
2026-02-10 22:02:01 +0300

    22

--- craft/variants.js
+++ craft/variants.js
@@ -45,5 +45,40 @@ window.CRAFT_VARIANTS = {
     "/YourWorld-Guide/images/knife/tactical_8.png",
     "/YourWorld-Guide/images/knife/tactical_9.png",
     "/YourWorld-Guide/images/knife/tactical_10.png"
+  ],
+  "fixer_set": [
+    "/YourWorld-Guide/images/sets/setbox_fixer.png",
+    "/YourWorld-Guide/images/sets/setbox_fixer_black.png",
+    "/YourWorld-Guide/images/sets/setbox_fixer_camo.png"
+  ],
+  "fixer_helmet": [
+    "/YourWorld-Guide/images/clothing/fixer_helmet.png",
+    "/YourWorld-Guide/images/clothing/fixer_helmet_black.png",
+    "/YourWorld-Guide/images/clothing/fixer_helmet_camo.png"
+  ],
+  "fixer_vest": [
+    "/YourWorld-Guide/images/clothing/fixer_vest.png",
+    "/YourWorld-Guide/images/clothing/fixer_vest_black.png",
+    "/YourWorld-Guide/images/clothing/fixer_vest_camo.png"
+  ],
+  "fixer_jacket": [
+    "/YourWorld-Guide/images/clothing/fixer_jacket.png",
+    "/YourWorld-Guide/images/clothing/fixer_jacket_black.png",
+    "/YourWorld-Guide/images/clothing/fixer_jacket_camo.png"
+  ],
+  "fixer_pants": [
+    "/YourWorld-Guide/images/clothing/fixer_pants.png",
+    "/YourWorld-Guide/images/clothing/fixer_pants_black.png",
+    "/YourWorld-Guide/images/clothing/fixer_pants_camo.png"
+  ],
+  "fixer_gloves": [
+    "/YourWorld-Guide/images/clothing/fixer_gloves.png",
+    "/YourWorld-Guide/images/clothing/fixer_gloves_black.png",
+    "/YourWorld-Guide/images/clothing/fixer_gloves_camo.png"
+  ],
+  "fixer_boots": [
+    "/YourWorld-Guide/images/clothing/fixer_boots.png",
+    "/YourWorld-Guide/images/clothing/fixer_boots_black.png",
+    "/YourWorld-Guide/images/clothing/fixer_boots_camo.png"
   ]
 };
